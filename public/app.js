2f8ae0db861466cc8a2d289279843dd2197e4a2f
yamamoto yuta <ftbae1@gmail.com>
2017-10-19 00:12:39 +0900

    add some test

--- public/app.js
+++ public/app.js
@@ -1,7 +1,20 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = function (problemNumber) {
+    var title = 'Problem #' + problemNumber + ' Coming soon!';
+    return $('<div class="problem-view">').text(title);
+};
+
 learnjs.showView = function (hash) {
-    var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView);
+    var routes = {
+        '#problem': learnjs.problemView
+    };
+
+    var hashParts = hash.split('-');
+    var viewFn = routes[hashParts[0]];
+
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn(hashParts[1]));
+    }
 };
