b9fc06e330691ea30cc840e6c1371536aa9f1643
yamamoto-yuta <yamamoto-yuta@dmm.com>
2017-10-23 14:46:13 +0900

    finished 3.3.2

--- public/app.js
+++ public/app.js
@@ -18,6 +18,24 @@ learnjs.applyObject = function (obj, elem) {
     }
 };
 
+learnjs.template = function (name) {
+    return $('.templates .' + name).clone();
+};
+
+learnjs.buildCorrectFlash = function (problemNum) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+
+    if (problemNum < learnjs.problems.length) {
+        link.attr('href', "#problem-" + (problemNum + 1));
+    } else {
+        link.attr('href', '');
+        link.text('You are Finished');
+    }
+
+    return correctFlash;
+};
+
 learnjs.problemView = function (data) {
     var problemNumber = parseInt(data, 10);
     var view = $('.templates .problem-view').clone();
@@ -32,7 +50,8 @@ learnjs.problemView = function (data) {
 
     function checkAnswerClick() {
         if (checkAnswer()) {
-            learnjs.flashElement(resultFlash, 'Correct!');
+            var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+            learnjs.flashElement(resultFlash, correctFlash);
         } else {
             learnjs.flashElement(resultFlash, 'Incorrect!');
         }
