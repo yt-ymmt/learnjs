95238e6c2fe32bd2ecd2f65bf08cfdfa4b62391e
yamamoto yuta <ftbae1@gmail.com>
2017-11-06 01:52:33 +0900

    add 6.3

--- services/lib/deploy.js
+++ services/lib/deploy.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 var fs = require('fs');
 
-AWS.config.region = 'us-east-1'
+AWS.config.region = 'ap-northeast-1';
 var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 var params = {
   FunctionName: 'learnjs_popularAnswers',
